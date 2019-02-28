2cd26c9c3eab341a5276ae7d6cce1325fe33a01a
azharnazli <mazhar.nazli@gmail.com>
2019-03-01 04:00:07 +0700

    finish fight

--- helper/helper.js
+++ helper/helper.js
@@ -28,26 +28,43 @@ function fight(player,monster){
     let monsterPower;
     let result;
     if(monster.ThreatId == 5) {
-        monsterPower = Math.ceil(Math.random()*(130-50))+50
+        monsterPower = Math.ceil(Math.random()*(80-20))+20
     } else if (monster.ThreatId == 4) {
-        monsterPower = Math.ceil(Math.random()*(1300-500))+500
+        monsterPower = Math.ceil(Math.random()*(200-100))+100
     } else if (monster.ThreatId == 3) {
-        monsterPower = Math.ceil(Math.random()*(13000-5000))+5000
+        monsterPower = Math.ceil(Math.random()*(5000-1000))+1000
     } else if (monster.ThreatId == 2) {
-        monsterPower = Math.ceil(Math.random()*(130000-50000))+50000
+        monsterPower = Math.ceil(Math.random()*(10000-3000))+3000
     } else if (monster.ThreatId == 1) {
-        monsterPower = Math.ceil(Math.random()*(1300000-500000))+500000
+        monsterPower = Math.ceil(Math.random()*(50000-5000))+5000
     }
     result =  player.powerLevel - monsterPower
-    if(result < 0 ) {
+    if(result <= 0 ) {
         result = "lose"
     } else {
-        result = win
+        result = "win"
     }
     return result
 }
 
+function checkClass(data){
+    let clas
+    if(data.powerLevel<=60){
+        clas = 4
+    } else if (data.powerLevel<=100) {
+        clas = 3
+    } else if (data.powerLevel<=5500) {
+        clas = 2
+    } else {
+        clas = 1
+    }
+    return clas
+}
+
+
 module.exports = {
     quizResult,
-    randomNumber
+    randomNumber,
+    fight,
+    checkClass
 }
