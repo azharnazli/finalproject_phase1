0f2d159cd4986bfd1a26be92cc72da902eb43080
azharnazli <mazhar.nazli@gmail.com>
2019-02-28 20:24:16 +0700

    fix session

--- routes/hero_routes.js
+++ routes/hero_routes.js
@@ -1,32 +1,47 @@
-const {Hero,Rank,Threat,Question,Answer} = require('../models')
+const {
+  Hero,
+  Rank,
+  Threat,
+  Question,
+  Answer
+} = require('../models')
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
-  res.render('hero/hero')
+router.get('/', (req, res) => {
+  if (req.session.login) {
+    res.render('hero/hero', {
+      session: req.session.login
+    })
+  } else {
+    res.redirect('/')
+  }
 })
 
-router.get('/quiz',(req,res)=>{
+router.get('/quiz', (req, res) => {
   Question.findAll({
-    include : [Answer]
-  })
-  .then(questions=>{
-    // res.send(questions)
-    res.render('hero/quiz',{questions})
-  })
-  .catch(err=>{
-    res.send(err.message)
-  })
+      include: [Answer]
+    })
+    .then(questions => {
+      // res.send(questions)
+      res.render('hero/quiz', {
+        questions
+      })
+    })
+    .catch(err => {
+      res.send(err.message)
+    })
 })
 
-router.post('/quiz',(req,res)=>{
+router.post('/quiz', (req, res) => {
   res.send(req.body)
 })
 
-router.post('/quiz-result',(req,res)=>{
-  res.render('quiz-result.ejs',{data:req.body})
+router.post('/quiz-result', (req, res) => {
+  res.render('quiz-result.ejs', {
+    data: req.body
+  })
 })
 
 
 module.exports = router
-        
