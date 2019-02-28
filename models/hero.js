336fa4074b9de5814120390e61e1ae06e7a12544
martinsuhendra <suhendra.martin90@gmail.com>
2019-02-28 22:22:16 +0700

    lets Fight

--- models/hero.js
+++ models/hero.js
@@ -7,11 +7,31 @@ module.exports = (sequelize, DataTypes) => {
     password : DataTypes.STRING,
     gender: DataTypes.STRING,
     email: DataTypes.STRING
-  }, {});
+  }, {
+    hooks : {
+      beforeUpdate : function(Hero){
+        if(Hero.powerLevel <= 60) {
+          return Hero.RankId = 4
+        }else if(Hero.powerLevel <= 100){
+          return Hero.RankId = 3
+        }
+      }
+    }
+  });
   Hero.associate = function(models) {
     // associations can be defined here
     Hero.hasOne(models.Rank)
     Hero.belongsToMany(models.Threat,{through:models.HeroesThreats})
   };
+
+  Hero.prototype.updateRank = function (){
+    const rank = 0;
+    if(this.powerLevel < 60){
+      rank == 4
+    }else if(this.powerLevel < 100){
+      rank == 3
+    }
+    return rank
+  }
   return Hero;
 };
